f8963510d590f2583e94c5daf09f8136da881412
Wesley Springer <wspringe@outlook.com>
2023-12-15 16:16:04 -0700

    Initial Commit

---
+++ test.bat
@@ -0,0 +1,39 @@
+@ECHO OFF
+TITLE Installing mod files
+setlocal ENABLEEXTENSIONS
+setlocal ENABLEDELAYEDEXPANSION
+
+set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Valve\Steam"
+set VALUE_NAME=InstallPath
+
+FOR /F "tokens=2* skip=2" %%a in ('reg query "%KEY_NAME%" /v "%VALUE_NAME%"') do (
+   set InstallPath=%%b
+   @echo InstallPath: !InstallPath!
+)
+
+set LibraryFilePath="!InstallPath!\steamapps\"
+pushd !LibraryFilePath!
+
+FOR /F "usebackq tokens=1* delims=		" %%a IN ("libraryfolders.vdf") DO (
+    echo %%a|find "path" > nul
+    if not errorlevel 1 (
+       set SteamGamesPath=%%b
+       @echo !SteamGamesPath!
+
+       if exist "!SteamGamesPath!\steamapps\common\Lethal Company\" (
+         set LethalCompanyInstallPath="!SteamGamesPath!\steamapps\common\Lethal Company\"
+       )
+    )
+)
+popd
+
+@echo %cd%
+
+if exist !LethalCompanyInstallPath! (
+    @echo yep
+    xcopy /s %cd%\*.txt !LethalCompanyInstallPath!\BepInEx\plugins\ /Y
+    PAUSE
+) else (
+    @echo Did not find an install path for Lethal Company
+    PAUSE
+)
