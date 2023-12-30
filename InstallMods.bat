87d43e2288ea3992a31ab9c0cdd4d2aeb6b8831a
Wesley Springer <wspringe@outlook.com>
2023-12-29 20:39:31 -0700

    feat: add config file copying

--- InstallMods.bat
+++ InstallMods.bat
@@ -8,19 +8,21 @@ set VALUE_NAME=InstallPath
 
 FOR /F "tokens=2* skip=2" %%a in ('reg query "%KEY_NAME%" /v "%VALUE_NAME%"') do (
    set InstallPath=%%b
-   @echo InstallPath: !InstallPath!
+   @echo Steam installation path: !InstallPath!
 )
 
 set LibraryFilePath="!InstallPath!\steamapps\"
 pushd !LibraryFilePath!
 
+@echo Checking the libraryfolders.vdf file for your steam game install path(s)...
 FOR /F "usebackq tokens=1* delims=		" %%a IN ("libraryfolders.vdf") DO (
     echo %%a|find "path" > nul
     if not errorlevel 1 (
        set SteamGamesPath=%%b
-       @echo !SteamGamesPath!
+       @echo Found steam games install folder: !SteamGamesPath!
 
        if exist "!SteamGamesPath!\steamapps\common\Lethal Company\" (
+        @echo Found the lethal company install path: !SteamGamesPAth!\steamapps\common\Lethal Company\
          set LethalCompanyInstallPath="!SteamGamesPath!\steamapps\common\Lethal Company\"
        )
     )
@@ -28,8 +30,9 @@ FOR /F "usebackq tokens=1* delims=		" %%a IN ("libraryfolders.vdf") DO (
 popd
 
 if exist !LethalCompanyInstallPath! (
-    xcopy "%cd%\plugins\" !LethalCompanyInstallPath!\BepInEx\plugins\ /E /C /I /Y
-    PAUSE
+    @echo Copying the plugins and config folders
+    if exist "%cd%\plugins" xcopy "%cd%\plugins\" !LethalCompanyInstallPath!\BepInEx\plugins\ /E /C /I /Y
+    if exist "%cd%\config\" xcopy "%cd%\config\" !LethalCompanyInstallPath!\BepInEx\config\ /E /C /I /Y
 ) else (
     @echo Did not find an install path for Lethal Company
     PAUSE
